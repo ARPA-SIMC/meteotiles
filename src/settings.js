c27e304e5452de7303e4cafa6ba8878c6f9e4eca
Emanuele Di Giacomo <edigiacomo@arpae.it>
2023-02-20 11:16:50 +0100

    v0.12

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-export const VERSION = "0.11";
+export const VERSION = "0.12";
 export const TILES_SERVER_URL = "./tiles";
 export const MAX_PRODUCTS_SELECTED = 4;
 export const USE_GRID_DEBUG = false;
