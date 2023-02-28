6c7029d6316d458e99b8542d2478423bca984a6e
Emanuele Di Giacomo <edigiacomo@arpae.it>
2023-02-28 17:04:03 +0100

    Disabled (again) map sync in four maps interface

--- src/fourmaps.js
+++ src/fourmaps.js
@@ -193,20 +193,20 @@ productListB.fetchProducts();
 productListC.fetchProducts();
 productListD.fetchProducts();
 
-productListA.registerOnProductsLoaded(() => {
-    mapControllerA.getLeafletMap().sync(mapControllerB.getLeafletMap());
-    mapControllerA.getLeafletMap().sync(mapControllerC.getLeafletMap());
-    mapControllerA.getLeafletMap().sync(mapControllerD.getLeafletMap());
+mapControllerA.getLeafletMap().sync(mapControllerB.getLeafletMap());
+mapControllerA.getLeafletMap().sync(mapControllerC.getLeafletMap());
+mapControllerA.getLeafletMap().sync(mapControllerD.getLeafletMap());
 
-    mapControllerB.getLeafletMap().sync(mapControllerA.getLeafletMap());
-    mapControllerB.getLeafletMap().sync(mapControllerC.getLeafletMap());
-    mapControllerB.getLeafletMap().sync(mapControllerD.getLeafletMap());
+/*
+mapControllerB.getLeafletMap().sync(mapControllerA.getLeafletMap());
+mapControllerB.getLeafletMap().sync(mapControllerC.getLeafletMap());
+mapControllerB.getLeafletMap().sync(mapControllerD.getLeafletMap());
 
-    mapControllerC.getLeafletMap().sync(mapControllerA.getLeafletMap());
-    mapControllerC.getLeafletMap().sync(mapControllerB.getLeafletMap());
-    mapControllerC.getLeafletMap().sync(mapControllerD.getLeafletMap());
+mapControllerC.getLeafletMap().sync(mapControllerA.getLeafletMap());
+mapControllerC.getLeafletMap().sync(mapControllerB.getLeafletMap());
+mapControllerC.getLeafletMap().sync(mapControllerD.getLeafletMap());
 
-    mapControllerD.getLeafletMap().sync(mapControllerA.getLeafletMap());
-    mapControllerD.getLeafletMap().sync(mapControllerB.getLeafletMap());
-    mapControllerD.getLeafletMap().sync(mapControllerC.getLeafletMap());
-});
+mapControllerD.getLeafletMap().sync(mapControllerA.getLeafletMap());
+mapControllerD.getLeafletMap().sync(mapControllerB.getLeafletMap());
+mapControllerD.getLeafletMap().sync(mapControllerC.getLeafletMap());
+*/
