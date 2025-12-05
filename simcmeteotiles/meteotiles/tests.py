b296927c516307446433752f736c54b8b672bb90
Emanuele Di Giacomo <edigiacomo@arpae.it>
2025-12-05 11:10:08 +0100

    Test

--- simcmeteotiles/meteotiles/tests.py
+++ simcmeteotiles/meteotiles/tests.py
@@ -1,39 +1,33 @@
 from pathlib import Path
 import tempfile
+from datetime import datetime, timezone
 
+from django.core.management import call_command
 from django.test import TestCase, override_settings
 from django.conf import settings
 
 from .models import WeatherModel, WeatherModelRun, WeatherProductRun
-from .utils import import_products
 
 
 class UtilsTestCase(TestCase):
     def setUp(self):
-        self.weather_model = WeatherModel(short_name="test", long_name="test_long_name")
-        self.weather_model.save()
         self.tempdir = tempfile.TemporaryDirectory(delete=False)
-        self.override = override_settings(METEOTILES_TILES_ROOT=self.tempdir.name)
+        self.override = override_settings(METEOTILES_BASEDIR=self.tempdir.name)
         self.override.enable()
 
     def tearDown(self):
         self.override.disable()
         self.tempdir.cleanup()
 
-    def test_import_products(self):
-        jsonfile = Path(__file__).parent / "testdata" / "products.json"
-        import_products(self.weather_model.short_name, jsonfile)
-        self.assertEqual(
-            WeatherModelRun.objects.count(), 1, "Only one WeatherModelRun was created"
-        )
-        self.assertEqual(
-            WeatherProductRun.objects.count(), 50, "50 WeatherProductRun  were imported"
-        )
-        capeshear = WeatherProductRun.objects.get(
-            short_name="capeshear_ita_small_tiles"
-        )
-        self.assertEqual(
-            capeshear.forecast_steps,
-            list(range(0, 90, 3)) + list(range(90, 241, 6)),
-            "Forecast steps of CAPE shear were imported",
-        )
+    def test_import_arkimaps_products(self):
+        weather_model = WeatherModel(short_name='icon_2I', long_name='ICON-2I', public=True)
+        weather_model.full_clean()
+        weather_model.save()
+        inputdir = settings.BASE_DIR.parent / Path('testdata/data/20250903_t')
+        call_command('meteotiles-import-arkimaps-products', 'icon_2I', inputdir)
+        self.assertEqual(WeatherModelRun.objects.count(), 1)
+        self.assertEqual(WeatherModelRun.objects.first().reftime, datetime(2025, 9, 3, tzinfo=timezone.utc))
+        self.assertEqual(WeatherModelRun.objects.first().weatherproductrun_set.count(), 1)
+        self.assertEqual(WeatherModelRun.objects.first().weatherproductrun_set.first().short_name, "tiles/t2m_emro_tiles")
+        self.assertEqual(WeatherModelRun.objects.first().weatherproductrun_set.first().long_name, "Temperature at 2 metres")
+        self.assertEqual(WeatherModelRun.objects.first().weatherproductrun_set.first().forecast_steps, [0, 1])
