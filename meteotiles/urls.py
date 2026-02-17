b58a2964772273d3dc9840c7956779ae027a3fb0
Emanuele Di Giacomo <edigiacomo@arpae.it>
2026-02-17 16:27:45 +0100

    index

--- meteotiles/urls.py
+++ meteotiles/urls.py
@@ -5,6 +5,7 @@ from .views import api_weather_models_list, singlemap, doublemap
 
 urlpatterns = [
     path('weather_models', api_weather_models_list, name='weather_models'),
+    path('', singlemap, name='index'),
     path('singlemap', singlemap, name='singlemap'),
     path('doublemap', doublemap, name='doublemap'),
 ]
